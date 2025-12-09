8dddb8c1924a0819346dd4fe84233c7581046678
Marcelo Galas <marcelo.galas@yahoo.com>
2025-12-09 15:33:41 -0600

    Refactor extension installation to use GitHub releases for versioning

--- includes.container/customizations/install-gnome-extensions.sh
+++ includes.container/customizations/install-gnome-extensions.sh
@@ -10,8 +10,10 @@ https://extensions.gnome.org/extension/19/user-themes/)
 for i in "${array[@]}"
 do
     EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
-    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
-    wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
+    EXTENSION_URL=$(curl -s $i | grep -oP '(?<=id="extension_url">)[^<]+')
+    EXTENSION_REPO=$(echo $EXTENSION_URL | sed 's/https:\/\/github.com\///')
+    EXTENSION_VERSION=$(curl -s https://api.github.com/repos/$EXTENSION_REPO/releases/latest | jq -r .tag_name)
+    wget "https://github.com/$EXTENSION_REPO/releases/download/$EXTENSION_VERSION/$EXTENSION_ID.zip" -O "$EXTENSION_ID.zip"
     gnome-extensions install --force ${EXTENSION_ID}.zip
     if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
         busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
