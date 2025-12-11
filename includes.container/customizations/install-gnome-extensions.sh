e790e5577c15608c9f2f4c9781b6055dc5087d74
Marcelo Galas <marcelo.galas@yahoo.com>
2025-12-11 13:19:02 -0600

    Remove Date Menu Formatter extension files
    - Deleted metadata.json, prefs.js, and related schema files for the Date Menu Formatter extension.
    - Removed utility files including formatter.js, general.js, markup.js, and prefFields.js.
    - Updated recipe.yml to remove unnecessary gnome-shell extensions dependencies.

--- includes.container/customizations/install-gnome-extensions.sh
+++ includes.container/customizations/install-gnome-extensions.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -e
-array=( https://extensions.gnome.org/extension/4655/date-menu-formatter/ )
+array=( https://extensions.gnome.org/extension/615/appindicator-support/
+https://extensions.gnome.org/extension/4655/date-menu-formatter/
+https://extensions.gnome.org/extension/3628/arcmenu/
+https://extensions.gnome.org/extension/3193/blur-my-shell/
+https://extensions.gnome.org/extension/1160/dash-to-panel/
+https://extensions.gnome.org/extension/19/user-themes/)
 
 for i in "${array[@]}"
 do
+    PK=$(basename "$(dirname "$i")")
     EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
-    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-info?pk=$PK" \
+        | jq -r --arg pk "$PK" '.shell_version_map | map(.pk) | max')
+    echo "Downloading $EXTENSION_ID version $VERSION_TAG with PK $PK"
     wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
-    mkdir -p /usr/share/gnome-shell/extensions/${EXTENSION_ID}
-    unzip -o ${EXTENSION_ID}.zip -d /usr/share/gnome-shell/extensions/${EXTENSION_ID}
+    gnome-extensions install --force ${EXTENSION_ID}.zip
+    if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
+        busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
+    fi
+    gnome-extensions enable ${EXTENSION_ID}
     rm ${EXTENSION_ID}.zip
 done
